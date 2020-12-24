f184e2d4c768590fc3dd6dff41b11b7d3b25b1b8
Tarun Sastry <60286295+tarunsas3@users.noreply.github.com>
2020-12-24 15:58:21 +0530

    Create script.js

---
+++ Rest Countries/Array Filter/Asia Region Task/script.js
@@ -0,0 +1,16 @@
+fetch("https://restcountries.eu/rest/v2/all")
+  .then((res) => res.json())
+  .then((data) => initialize(data))
+  .catch((err) => console.log(err));
+
+function initialize(countryData) {
+  countries = countryData;
+  for (let i = 0; i < countries.length; i++) {
+    result = countries.filter(function (e) {
+      return e.region == "Asia";
+    });
+  }
+  for (let i = 0; i < result.length; i++) {
+    console.log(result[i].name);
+  }
+}
